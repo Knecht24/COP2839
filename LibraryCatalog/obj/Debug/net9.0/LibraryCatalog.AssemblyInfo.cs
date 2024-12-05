8d413769566958883d448bd99690bb894030612a
Knecht24 <jhn.tyl.lve@gmail.com>
2024-12-04 21:12:34 -0600

    databaseDelete

--- LibraryCatalog/obj/Debug/net9.0/LibraryCatalog.AssemblyInfo.cs
+++ LibraryCatalog/obj/Debug/net9.0/LibraryCatalog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryCatalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ce40a86703da9e905828dafa228f51ca6d323a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12064a1a35073575add3d5ef66aa0a919b10f815")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryCatalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryCatalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
